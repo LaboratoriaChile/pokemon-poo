9b3980daf4d37e2d1019a13bab1327886a05fa9f
Blanca Pérez <blankp@gmail.com>
2017-05-05 10:57:36 -0300

    Added new fight feature and filled sect with all pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var pokemon = [];
+
 function Pokemon(nombre, color, poderDeAtaque, vida){
 	this.nombre = nombre;
 	this.color = color;
@@ -17,12 +19,53 @@ function Pokemon(nombre, color, poderDeAtaque, vida){
 		pokemon.vida -= this.poderDeAtaque;
 	}
 
-
 }
 
-var pikachu = new Pokemon("Pikachu", "amarillo", 30, 60);
-var charmander = new Pokemon("Charmander", "naranja", 30, 50);
+function principal(){
+	
 
-console.log("La vida de Charmander es: " + charmander.vida);
-pikachu.atacar(charmander);
-console.log("Después del ataque la vida de charmandder es: " + charmander.vida);
+	var pikachu = new Pokemon("Pikachu", "amarillo", 30, 60);
+	var charmander = new Pokemon("Charmander", "naranja", 30, 50);
+	var squirtle = new Pokemon("Squirtle", "azul", 20, 50);
+	var bulbasaur = new Pokemon("Bulbasaur", "verde", 20, 50 );
+	pokemon.push(pikachu);
+	pokemon.push(charmander);
+	pokemon.push(squirtle);
+	pokemon.push(bulbasaur);
+
+	llenarSelect("pokemon1", pokemon);
+	llenarSelect("pokemon2", pokemon);
+}
+
+function llenarSelect(idSelect, arrayPokemon){
+	var select = document.getElementById(idSelect);
+	var opAux;
+	arrayPokemon.forEach(function(elemento){
+		opAux = document.createElement("option");
+		opAux.innerHTML =  elemento.nombre;
+		select.appendChild(opAux);
+	}); 
+}
+
+function pelear(){
+	var pok1 = document.getElementById("pokemon1").value;
+	var pok2 = document.getElementById("pokemon2").value;
+	var res = document.getElementById("resultados");
+	
+	var pokemon1 = pokemon.filter(function(el){
+		return el.nombre == pok1;
+	})[0];
+	var pokemon2 = pokemon.filter(function(el){
+		return el.nombre == pok2;
+	})[0];
+
+	if(pokemon1 === pokemon2){
+		alert("Escoja dos pokemon diferentes");
+	}else if(pokemon2.vida <= 0){
+		alert(pokemon2.nombre + " no se puede atacar");
+	}else{
+		pokemon1.atacar(pokemon2);
+		res.innerHTML = pokemon1.nombre + " atacó a " + pokemon2.nombre + " y " + pokemon2.nombre + " tiene una vida de " + pokemon2.vida;
+	}
+}
+principal();
